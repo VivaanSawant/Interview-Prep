71e372612ce6293f4c986ac6248981d1dc9b918f
VivaanSawant <vivaansawant08@gmail.com>
2025-05-05 22:27:45 -0400

    wgat te heck is this languag

--- Learning Python/start.py
+++ Learning Python/start.py
@@ -1,2 +1,24 @@
-print(16 ** (1 / 2));
-print("hello my name is vivaan\'s");
+print(16 ** (1 / 2))
+print(10 * "hello my name is vivaan\'s" + "\n\nboi")
+
+curr = "hi";
+print(curr + " boi");
+
+print(r"\n\n\n\n");
+
+x = 10;
+
+x + 10;
+
+curr = "vivaan"
+
+print(curr[0])
+print(curr[5])
+print(curr[-1])
+print(curr[-6])
+
+print(curr[0 : 6])
+print(curr[0 :])
+
+
+
