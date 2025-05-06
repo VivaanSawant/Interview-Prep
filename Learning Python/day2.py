0405d73e4d9f76d63e86f9d8b241009452b8dc62
VivaanSawant <vivaansawant08@gmail.com>
2025-05-06 16:22:02 -0400

    a

--- Learning Python/day2.py
+++ Learning Python/day2.py
@@ -10,3 +10,18 @@ dawg.insert(1, 12)
 
 print(dawg)
 
+#review over
+
+tup = (12, dawg, 14)
+print(tup)
+
+dawg.pop(len(dawg) - 1)
+print(tup)
+
+print(tup.count)
+
+S = {12, 14, 25, 12}
+
+print(S) 
+#can't index in a set
+
