b9cc73750511f424144870aec3d87b19b647f232
VivaanSawant <vivaansawant08@gmail.com>
2025-05-05 22:36:13 -0400

    fake langage

--- Learning Python/start.py
+++ Learning Python/start.py
@@ -19,6 +19,33 @@ print(curr[-6])
 
 print(curr[0 : 6])
 print(curr[0 :])
+print(curr[: 3])
+print(len(curr))
+
+arr = [12, "ooga", 2222, curr];
+
+print(arr[0])
+print(arr[1])
+print(arr[2])
+print(arr[3])
 
 
+print("\n\n" + arr[-1]);
 
+newlist = [arr, 12];
+
+print(newlist[0]);
+print(newlist[1]);
+
+newlist.append("hiiii")
+print(newlist[len(newlist) - 1])
+
+print(newlist[0])
+newlist.reverse
+print(newlist[0])
+
+print(newlist)
+
+newlist.insert(1, "WEEEE")
+
+print(newlist)
