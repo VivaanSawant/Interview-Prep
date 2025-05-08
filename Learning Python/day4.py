fee7e8f4debb40bf3815496065c4fa325afda1f8
VivaanSawant <vivaansawant08@gmail.com>
2025-05-08 17:06:07 -0400

    we back to PAITOOOON

---
+++ Learning Python/day4.py
@@ -0,0 +1,13 @@
+# REVIEW
+
+class Human:
+    def __init__(self, hands):
+        self.hands = hands;
+    def getHands(self):
+        return self.hands
+    
+
+Vivaan = Human(4);
+
+print(Vivaan.getHands())
+
