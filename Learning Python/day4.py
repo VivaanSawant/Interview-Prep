142ff34416797b82feb27a06a9e34c3ff2db9558
VivaanSawant <vivaansawant08@gmail.com>
2025-05-08 17:35:41 -0400

    inheritance shi

--- Learning Python/day4.py
+++ Learning Python/day4.py
@@ -52,3 +52,32 @@ lap1 = s1.lap;
 
 lap3 = Student.Laptop()
 s1.show()
+
+# inheritance
+
+class A:
+    def feature1(self):
+        print("Feature 1 working")
+    def feature2(self):
+        print("Feature 2 Working")
+        
+a1 = A()
+
+a1.feature1()
+a1.feature2()
+
+class B():
+    def feature3(self):
+        print("feature 3 working")
+    def feature4(self):
+        print("feature 4 working")
+
+class C(A, B):
+    def feature5(self):
+        print("feature 5 working")
+
+b1 = B()
+b1.feature3()
+
+c1 = C()
+c1.feature1()
