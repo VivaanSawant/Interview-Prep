ec8ff2dc7d4ffe0d36d02351e22364bcc2761c68
VivaanSawant <vivaansawant08@gmail.com>
2025-05-23 18:38:00 -0400

    Update 33.py

--- Litcod/33.py
+++ Litcod/33.py
@@ -6,28 +6,28 @@ class Solution(object):
         :rtype: int
         """
         def binsearch(nums, lo, hi):
+            if lo > hi:
+                return -1
             if nums[lo] == target:
                 return lo
             if nums[hi] == target:
                 return hi
-            if lo >= hi:
-                return -1
 
             mid = lo + ((hi - lo) // 2)
 
             if nums[mid] == target:
                 return mid
-            
-            print(lo, mid, hi)
-            
-            if target < nums[lo]:
-                return binsearch(nums, mid + 1, hi)
-            elif target > nums[hi]:
-                return binsearch(nums, lo, mid)
-            else:
-                if target < nums[mid]:
-                    return binsearch(nums, lo, mid)
+
+         
+            if nums[lo] <= nums[mid]:
+                if nums[lo] <= target < nums[mid]:
+                    return binsearch(nums, lo, mid - 1)
                 else:
                     return binsearch(nums, mid + 1, hi)
+            else:
+                if nums[mid] < target <= nums[hi]:
+                    return binsearch(nums, mid + 1, hi)
+                else:
+                    return binsearch(nums, lo, mid - 1)
 
         return binsearch(nums, 0, len(nums) - 1)
