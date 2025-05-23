d4463f2de47f52700a520211c96be17df47e872b
VivaanSawant <vivaansawant08@gmail.com>
2025-05-23 18:23:46 -0400

    Create 33.py

---
+++ Litcod/33.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        def binsearch(nums, lo, hi):
+            if nums[lo] == target:
+                return lo
+            if nums[hi] == target:
+                return hi
+            if lo >= hi:
+                return -1
+
+            mid = lo + ((hi - lo) // 2)
+            
+            if target < nums[lo]:
+                return binsearch(nums, mid + 1, hi)
+            else:
+                return binsearch(nums, lo, mid)
+
+        return binsearch(nums, 0, len(nums) - 1)
